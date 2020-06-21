f972cf251c2fe06d5304b22501d674e129921a55
Molosh Nikita <nmiokliotsah@gmail.com>
2020-06-21 15:35:28 +0300

    set up webpack

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+import '../css/style.css';
+import * as $ from 'jquery';
+
+const navUl = $('nav ul');
+
+$(function () {
+    $('.button-menu').click( () => {
+
+        if (navUl.css('display') === 'none') {
+            navUl.slideDown();
+        } else {
+            navUl.slideUp();
+        }
+    });
+    $(window).resize( () => {
+        if ($(window).width() > 800) {
+            navUl.css('display', 'flex');
+        } else {
+            navUl.css('display', 'none');
+        }
+    });
+    $('.service-slider').slick({
+        arrows: false,
+        dots: true
+    });
+});
+
+AOS.init();
---
+++ webpack.config.js
@@ -0,0 +1,157 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const { loader } = require('mini-css-extract-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+const isDev = process.env.NODE_ENV === 'development';
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+
+    if (!isDev) {
+        config.minimizer = [
+            new OptimizeCssAssetsPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+    return config;
+}
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
+const cssLoaders = extra => {
+    const loaders = [
+        {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+                hmr: isDev,
+                reloadAll: true
+            }
+        },
+        'css-loader',
+        'postcss-loader'
+    ];
+
+    if (extra) {
+        loaders.push(extra);
+    }
+
+    return loaders;
+}
+const babelLoader = preset => {
+    const opt = {
+        presets: [
+            '@babel/preset-env'
+        ],
+        plugins: [
+            "@babel/plugin-proposal-class-properties"
+        ]
+    }
+
+    if (preset) {
+        opt.presets.push(preset);
+    }
+
+    return opt;
+}
+const jsLoaders = () => {
+    const loaders = [
+        {
+            loader: "babel-loader",
+            options: babelLoader()
+        }
+    ]
+
+    if (isDev) {
+        loaders.push('eslint-loader');
+    }
+    return loaders;
+}
+const plugins = () => {
+    const base = [
+        new HTMLWebpackPlugin({
+            template: './index.html',
+            minify: {
+                collapseWhitespace: !isDev
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: filename('css')
+        })
+    ];
+    if (!isDev) {
+        base.push(new BundleAnalyzerPlugin());
+    }
+
+    return base;
+}
+module.exports = {
+    context: path.join(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: ['@babel/polyfill', './js/index.js']
+    },
+    output: {
+        filename: filename('js'),
+        path: path.join(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js', '.png']
+    },
+    devServer: {
+        port: 4200,
+        hot: isDev
+    },
+    optimization: optimization(),
+    devtool: isDev ? 'source-map' : '',
+    plugins: plugins(),
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: isDev,
+                            reloadAll: true
+                        }
+                    },
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: 'img/[name][hash].[ext]'
+                    }
+                }]
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: cssLoaders('sass-loader')
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: jsLoaders()
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            }
+        ]
+    }
+}
